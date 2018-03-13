89b33d260657a5c6177cd46ea654f05d0606d34f
Mercykwazi <mercymncube3@gmail.com>
2018-03-13 14:31:13 +0200

    modified my form

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -4,29 +4,35 @@ import ReactDom from 'react-dom'
 class NameForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
-        this.handleSubmit = this.handleSubmit.bind(this);
+        // this.state = { value: '' };
+        // this.handleSubmit = this.handleSubmit.bind(this);
     }
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
+    // handleChange(event) {
+    //     this.setState({ value: event.target.value });
+    // }
+    // handleSubmit(event) {
+    //     alert('A name was submitted: ' + this.state.value);
+    //     event.preventDefault();
+    // }
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Name:
-            <input type="text" value={this.state.value} onChange=
-                        {this.handleChange} />
+            <input type="text"  />
                 </label><br/>
                 <label>
                    last Name:
-            <input type="text" value={this.state.value} onChange=
-                        {this.handleChange} />
+            <input type="text" />
                 </label>
+                <label>
+                    email:
+            <input type="text"  />
+                </label><br/>
+                <label>
+                    Name:
+            <input type="text"  />
+                </label><br/>
                 <input type="submit" value="Submit" />
             </form>
         )
