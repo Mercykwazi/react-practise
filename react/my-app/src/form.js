e7c79f40cf346b2c52ee0fa1bf98e1f6b88ad696
Mercykwazi <mercymncube3@gmail.com>
2018-03-14 09:16:30 +0200

    modified my file

--- react/my-app/src/form.js
+++ react/my-app/src/form.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+    constructor(){
+        super()
+        this.state={Name:"kwazi",LastName:"",email:"",Gender:""}
+    }
     render() {
         return (
+            <div>
+                {this.state.Name}
+              
             <form >
                 <label>
                     Name:
@@ -22,6 +29,8 @@ export default class Form extends React.Component {
                 </label><br />
                 <input type="submit" value="Submit" />
             </form>
+            </div>
         )
+      
     }
 }
